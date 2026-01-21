6fe954e80f7b0a3d3e7f4291c53e1a0f7f79a197
mahindralodhi1002-hub <mahindralodhi@gmail.com>
2026-01-22 00:08:17 +0530

    Fixed api key

--- auth-navbar.js
+++ auth-navbar.js
@@ -1,8 +1,6 @@
-// Firebase imports
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-// Firebase config
 const firebaseConfig = {
   apiKey: "ATzASyDghZLJnfS9PC_nNF1-EMv5hRr-TI0z-Nw",
   authDomain: "mahi-website-4df3d.firebaseapp.com",
@@ -12,16 +10,13 @@ const firebaseConfig = {
   appId: "1:558542822230:web:968a6ebd3b19e6070854f7"
 };
 
-// Init
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// Navbar buttons
 const loginBtn = document.getElementById("loginBtn");
 const signupBtn = document.getElementById("signupBtn");
 const logoutBtn = document.getElementById("logoutBtn");
 
-// Login state check
 onAuthStateChanged(auth, (user) => {
   if (user) {
     loginBtn.style.display = "none";
@@ -34,7 +29,6 @@ onAuthStateChanged(auth, (user) => {
   }
 });
 
-// Logout
 logoutBtn.addEventListener("click", () => {
   signOut(auth).then(() => {
     alert("Logged out successfully");
