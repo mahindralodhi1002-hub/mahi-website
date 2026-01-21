11c8dff92d49e7ee6f13a3ce8b3488f1f8aeb51f
mahindralodhi1002-hub <mahindralodhi@gmail.com>
2026-01-21 22:59:39 +0530

    New navbar with login/signup

--- auth.js
+++ auth.js
@@ -1,56 +1,53 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { 
-  getAuth, 
-  createUserWithEmailAndPassword, 
+  getAuth,
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
   signInWithEmailAndPassword,
-  sendEmailVerification
+  signOut
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-// YOUR FIREBASE CONFIG
-const firebaseConfig = {
-  apiKey: "ATzAsyDghZLnJfsP9C_nNFl-EMv5hRr-Tl0z-Nw",
-  authDomain: "mahi-website-4df3d.firebaseapp.com",
-  projectId: "mahi-website-4df3d",
-  storageBucket: "mahi-website-4df3d.appspot.com",
-  messagingSenderId: "558548222320",
-  appId: "1:558548222320:web:968a6edb3b19e60798547f"
-};
+import { firebaseConfig } from "./firebase-config.js";
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-// SIGNUP FUNCTION
-window.signup = function () {
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
+// SIGNUP
+window.signup = function() {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
 
   createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-
-      sendEmailVerification(userCredential.user);
-
-      alert("Account created! Verify your email.");
+    .then(userCredential => {
+      sendEmailVerification(auth.currentUser);
+      alert("Verification link sent to your email!\nPlease verify and then login.");
     })
-    .catch((error) => {
-      alert(error.message);
-    });
+    .catch(error => alert(error.message));
 };
 
-// LOGIN FUNCTION
-window.login = function () {
-  const email = document.getElementById("login_email").value;
-  const password = document.getElementById("login_password").value;
+// LOGIN
+window.login = function() {
+  let email = document.getElementById("login_email").value;
+  let password = document.getElementById("login_password").value;
 
   signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      if (!userCredential.user.emailVerified) {
-        alert("Please verify email before login.");
+    .then(userCredential => {
+
+      if (!auth.currentUser.emailVerified) {
+        alert("Please verify your email first!");
         return;
       }
-      alert("Login Successful!");
+
       window.location.href = "index.html";
     })
-    .catch((error) => {
-      alert("Login failed: " + error.message);
-    });
+    .catch(error => alert(error.message));
+};
+
+// LOGOUT
+window.logoutUser = function() {
+  signOut(auth).then(() => {
+    alert("Logged out!");
+    window.location.href = "login.html";
+  });
 };
---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "ATzAsyDghZLnJfsP9C_nNFl-EMv5hRr-Tl0z-Nw",
+  authDomain: "mahi-website-4df3d.firebaseapp.com",
+  projectId: "mahi-website-4df3d",
+  storageBucket: "mahi-website-4df3d.appspot.com",
+  messagingSenderId: "558548222320",
+  appId: "1:558548222320:web:968a6edb3b19e60798547f"
+};
